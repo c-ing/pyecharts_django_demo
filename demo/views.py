b96169ee2866046d4fdab16a4f8c6244d788932c
chendecheng <1765410986@qq.com>
2019-12-17 18:41:29 +0800

    readme,voken的views修改

--- demo/views.py
+++ demo/views.py
@@ -146,10 +146,10 @@ def index(request):
 
 
 def getWaveform(request):
-    csv_file = 'your file'
-    data = pd.read_csv(csv_file)
+    #csv_file = 'your file'
+    #data = pd.read_csv(csv_file)
     TOOLS = "hover,crosshair,pan,wheel_zoom,box_zoom,reset,save,box_select"
     picture = figure(width=1200, height=400, tools=TOOLS)
-    picture.line(data['order'], data['value'], color='blue', alpha=0.5)
+    picture.line([1,2,3,4,5], [6,7,2,4,5], color='blue', alpha=0.5)
     script, div = components(picture, CDN)
     return render(request, 'waveform.html', {'script': script, 'div': div})
